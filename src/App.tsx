623628d525e9c3baf6621383edc22d133d80ff42
Isaac Ayomi <Okunlolaa3@outlook.com>
2025-09-15 04:19:09 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { BrowserRouter, Routes } from "react-router-dom";
+
+function App() {
+  return <p>Setting up</p>;
+  // <BrowserRouter>
+  //   <Routes>
+  //     <Route />
+  //   </Routes>
+  // </BrowserRouter>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      outfit: ["Outfit", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        red: "#FC4747",
+        darkBlue: "#10141E",
+        grayishBlue: "#5A698F",
+        semiDarkBlue: "#161D2F",
+      },
+    },
+  },
+  plugins: [],
+};
